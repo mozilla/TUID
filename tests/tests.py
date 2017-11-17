194244e9c64c78533bcab476c686522c16b54370
berock212 <brockers212@hotmail.com>
2017-11-17 14:22:04 -0500

    Switched over to pytest

--- tests/tests.py
+++ tests/tests.py
@@ -1,21 +1,21 @@
 from tidservice import TIDService
-import unittest
+import pytest
+
 config = None
 
 
-class TestgrabTIDs(unittest.TestCase):
-
-    def setUp(self):
-        # MAKE AN INSTANCE OF THE SERVICE
+class TestgrabTIDs:
+    @classmethod
+    def setup_class(self):
         self.service = TIDService()
 
     def test_new_then_old(self):
         #delete database then run this test
         old = self.service.grab_tids("/testing/geckodriver/CONTRIBUTING.md", "6162f89a4838")
         new = self.service.grab_tids("/testing/geckodriver/CONTRIBUTING.md", "06b1a22c5e62")
-        self.assertEqual(len(new),len(old))
+        assert len(new)==len(old)
         for i in range(0,len(old)):
-            self.assertEqual(old[i],new[i])
+            assert old[i]==new[i]
 
     def test_tids_on_changed_file(self):
         # https://hg.mozilla.org/integration/mozilla-inbound/rev/a6fdd6eae583/taskcluster/ci/test/tests.yml
@@ -31,7 +31,7 @@ class TestgrabTIDs(unittest.TestCase):
         )
 
         # assertAlmostEqual PERFORMS A STRUCURAL COMPARISION
-        self.assertEqual(len(old_lines)-4,len(same_lines))
+        assert len(old_lines)-4==len(same_lines)
 
 
         # THE FILE HAS FOUR LINES REMOVED
@@ -42,39 +42,38 @@ class TestgrabTIDs(unittest.TestCase):
         )
 
         # EXPECTING
-        self.assertEqual(len(new_lines), len(old_lines)-4)
+        assert len(new_lines)== len(old_lines)-4
 
     def test_remove_file(self):
-        self.assertEqual(0,len(self.service.grab_tids("/third_party/speedometer/InteractiveRunner.html","e3f24e165618")))
+        assert 0==len(self.service.grab_tids("/third_party/speedometer/InteractiveRunner.html","e3f24e165618"))
 
     def test_generic_1(self):
         old = self.service.grab_tids("/gfx/ipc/GPUParent.cpp","a5a2ae162869")
         new = self.service.grab_tids("/gfx/ipc/GPUParent.cpp","3acb30b37718")
-        self.assertEqual(len(old),467)
-        self.assertEqual(len(new),476)
+        assert len(old)==467
+        assert len(new)==476
         for i in range(1,207):
-            self.assertEqual(old[i],new[i])
+            assert old[i]==new[i]
 
     def test_file_with_line_replacement(self):
         new = self.service.grab_tids("/python/mozbuild/mozbuild/action/test_archive.py","e3f24e165618")
         old = self.service.grab_tids("/python/mozbuild/mozbuild/action/test_archive.py","c730f942ce30")
-        self.assertEqual(653,len(new))
-        self.assertEqual(653,len(old))
+        assert 653==len(new)
+        assert 653==len(old)
         for i in range(0,600):
             if i==374 or i==376:
-                self.assertNotEqual(old[i],new[i])
+                assert old[i] != new[i]
             else:
-                self.assertEqual(old[i],new[i])
+                assert old[i]==new[i]
 
     def test_distant_rev(self):
         old = self.service.grab_tids("/python/mozbuild/mozbuild/action/test_archive.py","e3f24e165618")
         new = self.service.grab_tids("/python/mozbuild/mozbuild/action/test_archive.py","0d1e55d87931")
-        self.assertEqual(len(old),653)
-        self.assertEqual(len(new),653)
+        assert len(old)==653
+        assert len(new)==653
         for i in range(0,653):
-            self.assertEqual(new[i],old[i])
+            assert new[i]==old[i]
 
     def test_new_file(self):
         rev = self.service.grab_tids("/media/audioipc/server/src/lib.rs","a39241b3e7b1")
-        self.assertEqual(len(rev),636)
-        
+        assert len(rev)==636
