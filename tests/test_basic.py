498d2368dba6726f02bdc56b121444a724853b13
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-02-13 15:29:17 -0500

    whitespace fix test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
 
 import pytest
 
@@ -11,6 +14,7 @@ import sql
 from tidservice import TIDService
 
 config = None
+
 
 @pytest.fixture
 def service(new_db):
@@ -21,70 +25,76 @@ def service(new_db):
 
 
 def test_new_then_old(service):
-    #delete database then run this test
+    # delete database then run this test
     old = service.get_tids("/testing/geckodriver/CONTRIBUTING.md", "6162f89a4838")
     new = service.get_tids("/testing/geckodriver/CONTRIBUTING.md", "06b1a22c5e62")
-    assert len(new)==len(old)
-    for i in range(0,len(old)):
-        assert old[i]==new[i]
+    assert len(new) == len(old)
+    for i in range(0, len(old)):
+        assert old[i] == new[i]
+
 
 def test_tids_on_changed_file(service):
-    # https://hg.mozilla.org/integration/mozilla-inbound/rev/a6fdd6eae583/taskcluster/ci/test/tests.yml
-    old_lines = service.get_tids( # 2205 lines
-        "/taskcluster/ci/test/tests.yml","a6fdd6eae583"
+    # https://hg.mozilla.org/integration/mozilla-inbound/file/a6fdd6eae583/taskcluster/ci/test/tests.yml
+    old_lines = service.get_tids(  # 2205 lines
+        "/taskcluster/ci/test/tests.yml", "a6fdd6eae583"
     )
 
     # THE FILE HAS NOT CHANGED, SO WE EXPECT THE SAME SET OF TIDs AND LINES TO BE RETURNED
     # https://hg.mozilla.org/integration/mozilla-inbound/file/a0bd70eac827/taskcluster/ci/test/tests.yml
-    same_lines = service.get_tids( # 2201 lines
+    same_lines = service.get_tids(  # 2201 lines
 
-        "/taskcluster/ci/test/tests.yml","c8dece9996b7"
+        "/taskcluster/ci/test/tests.yml", "a0bd70eac827"
     )
 
     # assertAlmostEqual PERFORMS A STRUCURAL COMPARISION
-    assert len(old_lines)-4==len(same_lines)
-
+    assert len(old_lines) == len(same_lines)
 
     # THE FILE HAS FOUR LINES REMOVED
     # https://hg.mozilla.org/integration/mozilla-inbound/rev/c8dece9996b7
     # https://hg.mozilla.org/integration/mozilla-inbound/file/c8dece9996b7/taskcluster/ci/test/tests.yml
+    # 2201 lines
     new_lines = service.get_tids(
-        "/taskcluster/ci/test/tests.yml","c8dece9996b7"
+        "/taskcluster/ci/test/tests.yml", "c8dece9996b7"
     )
 
     # EXPECTING
-    assert len(new_lines)== len(old_lines)-4
+    assert len(new_lines) == len(old_lines) - 4
+
 
 def test_remove_file(service):
-    assert 0==len(service.get_tids("/third_party/speedometer/InteractiveRunner.html","e3f24e165618"))
+    assert 0 == len(service.get_tids("/third_party/speedometer/InteractiveRunner.html", "e3f24e165618"))
+
 
 def test_generic_1(service):
-    old = service.get_tids("/gfx/ipc/GPUParent.cpp","a5a2ae162869")
-    new = service.get_tids("/gfx/ipc/GPUParent.cpp","3acb30b37718")
-    assert len(old)==467
-    assert len(new)==476
-    for i in range(1,207):
-        assert old[i]==new[i]
+    old = service.get_tids("/gfx/ipc/GPUParent.cpp", "a5a2ae162869")
+    new = service.get_tids("/gfx/ipc/GPUParent.cpp", "3acb30b37718")
+    assert len(old) == 467
+    assert len(new) == 476
+    for i in range(1, 207):
+        assert old[i] == new[i]
+
 
 def test_file_with_line_replacement(service):
-    new = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py","e3f24e165618")
-    old = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py","c730f942ce30")
-    assert 653==len(new)
-    assert 653==len(old)
-    for i in range(0,600):
-        if i==374 or i==376:
+    new = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py", "e3f24e165618")
+    old = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py", "c730f942ce30")
+    assert 653 == len(new)
+    assert 653 == len(old)
+    for i in range(0, 600):
+        if i == 374 or i == 376:
             assert old[i] != new[i]
         else:
-            assert old[i]==new[i]
+            assert old[i] == new[i]
+
 
 def test_distant_rev(service):
-    old = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py","e3f24e165618")
-    new = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py","0d1e55d87931")
-    assert len(old)==653
-    assert len(new)==653
-    for i in range(0,653):
-        assert new[i]==old[i]
+    old = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py", "e3f24e165618")
+    new = service.get_tids("/python/mozbuild/mozbuild/action/test_archive.py", "0d1e55d87931")
+    assert len(old) == 653
+    assert len(new) == 653
+    for i in range(0, 653):
+        assert new[i] == old[i]
+
 
 def test_new_file(service):
-    rev = service.get_tids("/media/audioipc/server/src/lib.rs","a39241b3e7b1")
-    assert len(rev)==636
+    rev = service.get_tids("/media/audioipc/server/src/lib.rs", "a39241b3e7b1")
+    assert len(rev) == 636
