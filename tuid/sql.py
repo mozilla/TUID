66b15254f852cb19f972f4eef1766b215e9d6e98
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-03-27 17:33:48 -0400

    add transaction

--- tuid/sql.py
+++ tuid/sql.py
@@ -9,10 +9,10 @@ from pyLibrary.sql.sqlite import Sqlite, quote_value
 
 
 class Sql:
-    def __init__(self,dbname):
+    def __init__(self, dbname):
         self.db = Sqlite(dbname)
 
-    def execute(self,sql,params=None):
+    def execute(self, sql, params=None):
         if params:
             for p in params:
                 sql = sql.replace('?', quote_value(p), 1)
@@ -21,11 +21,37 @@ class Sql:
     def commit(self):
         self.db.commit()
 
-    def get(self,sql,params=None):
+    def get(self, sql, params=None):
         if params:
             for p in params:
                 sql = sql.replace('?', quote_value(p), 1)
         return self.db.query(sql).data
 
-    def get_one(self,sql,params=None):
+    def get_one(self, sql, params=None):
         return self.get(sql, params)[0]
+
+    def transaction(self):
+        return Transaction(self)
+
+
+class Transaction(object):
+    def __init__(self, db):
+        self.db = db
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if isinstance(exc_val, Exception):
+            self.db.rollback()
+        else:
+            self.db.commit()
+
+    def execute(self, sql, params=None):
+        return self.db.execute(sql, params)
+
+    def get(self, sql, params=None):
+        return self.db.get(sql, params)
+
+    def get_one(self, sql, params=None):
+        return self.db.getone(sql, params)
