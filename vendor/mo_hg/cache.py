1804b60221dd2b58eaa08eb1532c269c73279e7c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-07-18 15:25:06 -0400

    lib updates

--- vendor/mo_hg/cache.py
+++ vendor/mo_hg/cache.py
@@ -10,19 +10,24 @@
 from __future__ import division
 from __future__ import unicode_literals
 
-from flask import Response
+import json
 
+from flask import Response
 from mo_dots import coalesce
+from mo_files.url import URL
 from mo_future import text_type, xrange
-from mo_json import json2value, value2json
+from mo_json import value2json
+from mo_kwargs import override
 from mo_logs import Log
 from mo_threads import Lock, Signal, Queue, Thread, Till
 from mo_times import Date, SECOND, MINUTE
 from pyLibrary.env import http
 from pyLibrary.sql.sqlite import Sqlite, quote_value, quote_list
 
+from mo_hg.rate_logger import RateLogger
+
 APP_NAME = "HG Cache"
-CONCURRENCY = 1
+CONCURRENCY = 5
 AMORTIZATION_PERIOD = SECOND
 HG_REQUEST_PER_SECOND = 10
 CACHE_RETENTION = 10 * MINUTE
@@ -33,15 +38,31 @@ class Cache(object):
     For Caching hg.mo requests
     """
 
-    def __init__(self, rate=None, amortization_period=None, database=None):
-        self.amortization_period = coalesce(amortization_period.seconds, AMORTIZATION_PERIOD.seconds)
+    @override
+    def __init__(self, rate=None, amortization_period=None, source=None, database=None, kwargs=None):
+        self.amortization_period = coalesce(amortization_period, AMORTIZATION_PERIOD)
         self.rate = coalesce(rate, HG_REQUEST_PER_SECOND)
         self.cache_locker = Lock()
         self.cache = {}  # MAP FROM url TO (ready, headers, response, timestamp) PAIR
+        self.no_cache = {}  # VERY SHORT TERM CACHE
         self.workers = []
         self.todo = Queue(APP_NAME+" todo")
-        self.requests = Queue(APP_NAME+" requests", max=self.rate * self.amortization_period)
+        self.requests = Queue(APP_NAME + " requests", max=int(self.rate * self.amortization_period.seconds))
+        self.url = URL(source.url)
         self.db = Sqlite(database)
+        self.inbound_rate = RateLogger("Inbound")
+        self.outbound_rate = RateLogger("hg.mo")
+
+        if not self.db.query("SELECT name FROM sqlite_master WHERE type='table'").data:
+            with self.db.transaction() as t:
+                t.execute(
+                    "CREATE TABLE cache ("
+                    "   path TEXT PRIMARY KEY, "
+                    "   headers TEXT, "
+                    "   response TEXT, "
+                    "   timestamp REAL "
+                    ")"
+                )
 
         self.threads = [
             Thread.run(APP_NAME+" worker" + text_type(i), self._worker)
@@ -51,24 +72,28 @@ class Cache(object):
         self.cleaner = Thread.run(APP_NAME+" cleaner", self._cache_cleaner)
 
     def _rate_limiter(self, please_stop):
-        max_requests = self.requests.max
-        recent_requests = []
+        try:
+            max_requests = self.requests.max
+            recent_requests = []
 
-        while not please_stop:
-            now = Date.now()
-            too_old = now - self.amortization_period
+            while not please_stop:
+                now = Date.now()
+                too_old = now - self.amortization_period
 
-            recent_requests = [t for t in recent_requests if t > too_old]
+                recent_requests = [t for t in recent_requests if t > too_old]
 
-            num_recent = len(recent_requests)
-            if num_recent >= max_requests:
-                space_free_at = recent_requests[0] + self.amortization_period
-                (please_stop | Till(till=space_free_at.unix)).wait()
-                continue
-            for _ in xrange(num_recent, max_requests):
-                request = self.todo.pop()
-                recent_requests.append(Date.now())
-                self.requests.add(request)
+                num_recent = len(recent_requests)
+                if num_recent >= max_requests:
+                    space_free_at = recent_requests[0] + self.amortization_period
+                    (please_stop | Till(till=space_free_at.unix)).wait()
+                    continue
+                for _ in xrange(num_recent, max_requests):
+                    request = self.todo.pop()
+                    now = Date.now()
+                    recent_requests.append(now)
+                    self.requests.add(request)
+        except Exception as e:
+            Log.warning("failure", cause=e)
 
     def _cache_cleaner(self, please_stop):
         while not please_stop:
@@ -77,30 +102,35 @@ class Cache(object):
 
             remove = set()
             with self.cache_locker:
-                for url, (ready, headers, response, timestamp) in self.cache:
+                for path, (ready, headers, response, timestamp) in self.cache:
                     if timestamp < too_old:
-                        remove.add(url)
+                        remove.add(path)
                 for r in remove:
                     del self.cache[r]
             (please_stop | Till(seconds=CACHE_RETENTION.seconds / 2)).wait()
 
-    def no_cache(self, url):
+    def please_cache(self, path):
         """
-        :return: False if `url` is not to be cached
+        :return: False if `path` is not to be cached
         """
-        if url.endswith("/tip"):
+        if path.endswith("/tip"):
+            return False
+        if any(k in path for k in ["/json-annotate/", "/json-info/", "/json-log/", "/json-rev/", "/rev/", "/raw-rev/", "/raw-file/", "/json-pushes"]):
             return True
+
         return False
 
-    def request(self, method, url, headers):
+    def request(self, method, path, headers):
         now = Date.now()
-        ready = Signal(url)
+        self.inbound_rate.add(now)
+        ready = Signal(path)
 
         # TEST CACHE
         with self.cache_locker:
-            pair = self.cache.get(url)
+            pair = self.cache.get(path)
             if pair is None:
-                self.cache[url] = (ready, None, None, now)
+                self.cache[path] = (ready, None, None, now)
+
 
         if pair is not None:
             # REQUEST IS IN THE QUEUE ALREADY, WAIT
@@ -108,59 +138,71 @@ class Cache(object):
             if response is None:
                 ready.wait()
                 with self.cache_locker:
-                    ready, headers, response, timestamp = self.cache.get(url)
-            with self.db.transaction as t:
-                t.execute("UPDATE cache SET timestamp=" + quote_value(now) + " WHERE url=" + quote_value(url))
+                    ready, headers, response, timestamp = self.cache.get(path)
+            with self.db.transaction() as t:
+                t.execute("UPDATE cache SET timestamp=" + quote_value(now) + " WHERE path=" + quote_value(path) + " AND timestamp<" + quote_value(now))
             return Response(
                 response,
                 status=200,
-                headers=json2value(headers)
+                headers=json.loads(headers)
             )
 
         # TEST DB
-        db_response = self.db.query("SELECT headers, response FROM cache WHERE url=" + quote_value(url))
+        db_response = self.db.query("SELECT headers, response FROM cache WHERE path=" + quote_value(path)).data
         if db_response:
             headers, response = db_response[0]
-            with self.db.transaction as t:
-                t.execute("UPDATE cache SET timestamp=" + quote_value(now) + " WHERE url=" + quote_value(url))
+            with self.db.transaction() as t:
+                t.execute("UPDATE cache SET timestamp=" + quote_value(now) + " WHERE path=" + quote_value(path) + " AND timestamp<" + quote_value(now))
             with self.cache_locker:
-                self.cache[url] = (ready, headers, response, now)
+                self.cache[path] = (ready, headers, response.encode('latin1'), now)
             ready.go()
 
             return Response(
                 response,
                 status=200,
-                headers=json2value(headers)
+                headers=json.loads(headers)
             )
 
         # MAKE A NETWORK REQUEST
-        self.todo.add((ready, method, url, headers, now))
+        self.todo.add((ready, method, path, headers, now))
         ready.wait()
         with self.cache_locker:
-            ready, headers, response, timestamp = self.cache[url]
+            ready, headers, response, timestamp = self.cache[path]
         return Response(
             response,
             status=200,
-            headers=json2value(headers)
+            headers=json.loads(headers)
         )
 
-    def _worker(self, todo, please_stop):
+    def _worker(self, please_stop):
         while not please_stop:
             pair = self.requests.pop(till=please_stop)
             if please_stop:
                 break
-            ready, method, url, headers, timestamp = pair
+            ready, method, path, req_headers, timestamp = pair
 
             try:
-                response = http.request(method, url, headers)
-                with self.db.transaction as t:
-                    t.execute("INSERT INTO cache (url, headers, response, timestamp) VALUES" + quote_list((url, headers, response, timestamp)))
+                url = self.url / path
+                self.outbound_rate.add(Date.now())
+                response = http.request(method, url, req_headers)
+
+                del response.headers['transfer-encoding']
+                resp_headers = value2json(response.headers)
+                resp_content = response.raw.read()
+
+                please_cache = self.please_cache(path)
+                if please_cache:
+                    with self.db.transaction() as t:
+                        t.execute("INSERT INTO cache (path, headers, response, timestamp) VALUES" + quote_list((path, resp_headers, resp_content.decode('latin1'), timestamp)))
                 with self.cache_locker:
-                    self.cache[url] = (ready, response.content, value2json(response.headers), timestamp)
+                    self.cache[path] = (ready, resp_headers, resp_content, timestamp)
             except Exception as e:
-                Log.warning("problem with request to {{url}}", url=url, cause=e)
+                Log.warning("problem with request to {{path}}", path=path, cause=e)
                 with self.cache_locker:
-                    ready, headers, response = self.cache[url]
-                    del self.cache[url]
+                    ready, headers, response = self.cache[path]
+                    del self.cache[path]
             finally:
                 ready.go()
+
+
+
--- vendor/mo_hg/relay_app.py
+++ vendor/mo_hg/relay_app.py
@@ -4,8 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
-#
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
@@ -79,6 +77,8 @@ def add(any_flask_app):
     cache = Cache(config.cache)
     any_flask_app.add_url_rule(str('/<path:path>'), None, relay_get, methods=[str('GET')])
     any_flask_app.add_url_rule(str('/<path:path>'), None, relay_post, methods=[str('POST')])
+    any_flask_app.add_url_rule(str('/'), None, relay_get, methods=[str('GET')])
+    any_flask_app.add_url_rule(str('/'), None, relay_post, methods=[str('POST')])
 
 
 if __name__ in ("__main__",):
