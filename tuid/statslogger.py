b350c79820d8344ef0bc472749e78c08b8d7cbca
Greg Mierzwinski <gmierz1@live.ca>
2018-07-22 11:49:31 -0400

    Remove portions of memory logger for python 2.7.

--- tuid/statslogger.py
+++ tuid/statslogger.py
@@ -14,8 +14,6 @@ from mo_times.durations import MINUTE
 
 import linecache
 import os
-import tracemalloc
-import memory_profiler
 import psutil
 from datetime import datetime
 
@@ -93,64 +91,19 @@ class StatsLogger:
                 Log.warning("Unexpected error in pc-daemon: {{cause}}", cause=e)
 
 
-    def display_top(self, snapshot, key_type='lineno', limit=20):
-        top_stats = snapshot.statistics(key_type)
-
-        Log.note("Top {{num}} lines", num=limit)
-        for index, stat in enumerate(top_stats[:limit], 1):
-            frame = stat.traceback[0]
-            # replace "/path/to/module/file.py" with "module/file.py"
-            filename = os.sep.join(frame.filename.split(os.sep)[-2:])
-            Log.note(
-                "#{{num}}: {{fname}}:{{lineno}}: {{size}} KiB",
-                num=index,
-                fname=filename,
-                lineno=frame.lineno,
-                size=round(stat.size / 1024, 1)
-            )
-
-            line = linecache.getline(frame.filename, frame.lineno).strip()
-            if line:
-                Log.note("    {{line}}", line=line)
-
-        other = top_stats[limit:]
-        if other:
-            size = sum(stat.size for stat in other)
-            Log.note("{{others}} other: {{size}} KiB", others=len(other), size=round(size / 1024, 1))
-        total = sum(stat.size for stat in top_stats)
-        Log.note("Total allocated size: {{size}} KiB", size=round(total / 1024, 1))
-
-
     def get_free_memory(self):
         tmp = psutil.virtual_memory()
         return tmp.free
 
 
     def run_memory_daemon(self, please_stop):
-        tracemalloc.start()
-        old_max = 0
-        snapshot = None
-
         while not please_stop:
             try:
                 (Till(seconds=DAEMON_MEMORY_LOG_INTERVAL.seconds) | please_stop).wait()
-                max_rss = max(memory_profiler.memory_usage())
-                if max_rss > old_max:
-                    old_max = max_rss
-                    #snapshot = tracemalloc.take_snapshot()
-                    Log.note(
-                        "TUID Process - max-RSS {{maxrss}}",
-                        maxrss=max_rss
-                    )
-
                 mem = psutil.virtual_memory()
                 Log.note(
                     "TUID Process - complete memory info: {{mem}}",
                     mem=str(mem)
                 )
-
-                #if snapshot is not None:
-                #    Log.note("Displaying snapshot...")
-                #    self.display_top(snapshot)
             except Exception as e:
                 Log.warning("Error encountered while trying to log memory: {{cause}}", cause=e)
